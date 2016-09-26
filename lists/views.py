5ba69ac37830a6814e8ed5798073bc4eff39e292
Marek <marek.s.newton@gmail.com>
2016-09-26 09:54:23 -0600

    worked on views

--- lists/views.py
+++ lists/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import List, List_Item
 
 # Create your views here.
 def index(request):
-	return HttpResponse("hello world")
+	latest_lists = List.objects.all('-date')[:10]
+	context = {'latest_lists': latest_lists}
+	return render(request, 'lists/index.html', context)
 
 def list_all(request):
-	all_lists = List.objects.all()
-	return HttpResponse(all_lists)
+	pass	
 
 def add_list(request):
-	return HttpResponse("this is where you add lists")
+	return render(request, 'lists/add_list.html', {})
 
 def list(request, title):
 	return HttpResponse("this is a page for a list %s" % title)
